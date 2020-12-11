2d0c25cce83189bf9d78253fe7781a53627784b9
ashin <ashin.mahat@extensodata.com>
2020-12-11 20:03:39 +0545

    dsf

--- server.ts
+++ server.ts
@@ -6,10 +6,6 @@ import * as express from 'express';
 import {join} from 'path';
 import {readFileSync} from 'fs';
 
-import * as mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-const routeManager = require('./server/routes');
-
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
 
@@ -29,18 +25,6 @@ const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/mai
 import {ngExpressEngine} from '@nguniversal/express-engine';
 // Import module map for lazy loading
 import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
-
-// MongoDB database settings
-mongoose.connect('mongodb+srv://mahatashin:barcelona@cluster0-ykjjj.mongodb.net/sattaKing?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-})
-  .then(() => console.log('Database connected successfully!'))
-  .catch((err) => console.error(err));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 app.engine('html', ngExpressEngine({
